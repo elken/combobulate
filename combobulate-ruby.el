a6889228b49721631137533e96b41711e9a19eae
Ellis Kenyo <me@elken.dev>
2025-06-29 14:13:38 +0100

    Fix most remaining issues
    Navigation amount parent/child and siblings works now, as does splicing.

--- combobulate-ruby.el
+++ combobulate-ruby.el
@@ -27,14 +27,27 @@
 
 ;;; Code:
 
+(require 'combobulate-settings)
+(require 'combobulate-navigation)
+(require 'combobulate-interface)
+(require 'combobulate-rules)
+(require 'combobulate-setup)
+(require 'combobulate-manipulation)
+(declare-function combobulate--mark-node "combobulate-manipulation")
+(declare-function combobulate-indent-region "combobulate-manipulation")
+
 (defun combobulate-ruby--get-definition (node)
   (string-join
    (combobulate-query-node-text
     (pcase (combobulate-node-type node)
       ("method"
        '((_) name: (_) @name parameters: (_) @args))
+      ("singleton_method"
+       '((_) name: (_) @name parameters: (_) @args))
       ("class"
        '((_) name: (_) @name superclass: (constant) @args))
+      ("singleton_class"
+       '((_) value: (_) @name))
       ("module"
        '((_) name: (_) @name)))
     node
@@ -42,13 +55,16 @@
    ""))
 
 (defun combobulate-ruby-pretty-print-node-name (node default-name)
-  "Pretty printer for Ruby nodes"
+  "Pretty printer for Ruby nodes.
+Takes a NODE and prints DEFAULT-NAME if nothing found."
   (combobulate-string-truncate
    (replace-regexp-in-string
     (rx (| (>= 2 " ") "\n")) ""
     (pcase (combobulate-node-type node)
       ("method" (concat "def " (combobulate-ruby--get-definition node)))
+      ("singleton_method" (concat "def self." (combobulate-ruby--get-definition node)))
       ("class" (concat "class " (combobulate-ruby--get-definition node)))
+      ("singleton_class" (concat "class << " (combobulate-ruby--get-definition node)))
       ("module" (concat "module " (combobulate-ruby--get-definition node)))
       (_ default-name)))
    40))
@@ -57,18 +73,29 @@
   "Determine the next-closest indentation level to deindent to."
   (car-safe (last (seq-take-while (lambda (num) (< num (current-column)))
                                   (nconc (number-sequence (prog-first-column) (1- (ruby-calculate-indent))
-							  ruby-indent-level)
-					 (list (ruby-calculate-indent)))))))
+                                                          ruby-indent-level)
+                                         (list (ruby-calculate-indent)))))))
 
 (defun combobulate-ruby-calculate-indent (pos)
+  "Calculate the indentation for POS."
   (let ((calculated-indentation (save-excursion
                                   (goto-char pos)
                                   (combobulate-filter-nodes
                                    (combobulate-get-parents
-                                    (combobulate-node-at-point))))))
+                                    (combobulate-node-at-point))
+                                   :keep-types
+                                   '("block_body"
+                                     "body_statement"
+                                     "do"
+                                     "begin"
+                                     "class"
+                                     "module"
+                                     "method"
+                                     "singleton_method"
+                                     "singleton_class")))))
     (if (null calculated-indentation)
         (current-indentation)
-      (* (ruby-calculate-indent) (length calculated-indentation)))))
+      (* ruby-indent-level (length calculated-indentation)))))
 
 (eval-and-compile
   (defconst combobulate-ruby-definitions
@@ -77,181 +104,231 @@
       (pretty-print-node-name-function #'combobulate-ruby-pretty-print-node-name)
       (envelope-procedure-shorthand-alist
        '((calls . ((:activation-nodes
-		    ((:nodes ((rule "identifier") "call"))))))
-	 (iterables . ((:activation-nodes
-			((:nodes ((rule "array") (rule "range") (rule "hash") (rule "identifier") "call"))))))))
+                    ((:nodes ((rule "identifier") "call"))))))
+         (iterables . ((:activation-nodes
+                        ((:nodes ((rule "array") (rule "range") (rule "hash") (rule "identifier") "call"))))))))
       (envelope-list
        `((:description
-	  "if ... [else ...] end"
-	  :key "bi"
-	  :mark-node t
-	  :shorthand calls
-	  :name "nest-if-else"
-	  :template
-	  ((save-column
-	    "if " @ n>
-	    (choice* :name "if-block" :missing (@ "nil") :rest (r>))
-	    n)
-	   "else" n>
-	   (choice* :name "else-block" :missing (@ "nil" n>) :rest (r> n>))
-	   "end"))
-	 (:description
-	  "begin ... rescue ... end"
-	  :key "bbr"
-	  :mark-node t
-	  :shorthand calls
-	  :name "nest-begin-rescue"
-	  :template
-	  ((save-column
-	    @ "begin" n>
-	    (choice* :name "expression-block"
-		     :missing
-		     (@ "nil")
-		     :rest
-		     (r>))
-	    n)
-	   "rescue " (p StandardError "Exception" (lambda (in)
-						    (concat in " => e")))
-	   n>
-	   (choice* :name "handler-block"
-		    :missing
-		    (@ "nil" n>)
-		    :rest
-		    (r> n>))
-	   "end"))
-	 (:description
-	  "begin ... ensure ... end"
-	  :key "bbe"
-	  :mark-node t
-	  :shorthand calls
-	  :name "nest-begin-ensure"
-	  :template
-	  ((save-column
-	    @ "begin" n>
-	    (choice* :name "expression-block"
-		     :missing
-		     (@ "nil")
-		     :rest
-		     (r>))
-	    n)
-	   "ensure" n>
-	   (choice* :name "handler-block"
-		    :missing
-		    (@ "nil" n>)
-		    :rest
-		    (r> n>))
-	   "end"))
-	 (:description
-	  "begin ... rescue ... ensure ... end"
-	  :key "bre"
-	  :mark-node t
-	  :shorthand calls
-	  :name "nest-begin-rescue-ensure"
-	  :template
-	  ((save-column
-	    @ "begin" n>
-	    (choice* :name "expression-block"
-		     :missing
-		     (@ "nil")
-		     :rest
-		     (r>))
-	    n)
-	   "rescue " (p StandardError "Exception" (lambda (in)
-						    (concat in " => e")))
-	   n>
-	   (choice* :name "handler-block"
-		    :missing
-		    (@ "nil" n>)
-		    :rest
-		    (r> n>))
+          "if ... [else ...] end"
+          :key "bi"
+          :mark-node t
+          :shorthand calls
+          :name "nest-if-else"
+          :template
+          ((save-column
+            "if " @ n>
+            (choice* :name "if-block" :missing (@ "nil") :rest (r>))
+            n)
+           "else" n>
+           (choice* :name "else-block" :missing (@ "nil" n>) :rest (r> n>))
+           "end"))
+         (:description
+          "begin ... rescue ... end"
+          :key "bbr"
+          :mark-node t
+          :shorthand calls
+          :name "nest-begin-rescue"
+          :template
+          ((save-column
+            @ "begin" n>
+            (choice* :name "expression-block"
+                     :missing
+                     (@ "nil")
+                     :rest
+                     (r>))
+            n)
+           "rescue " (p StandardError "Exception" (lambda (in)
+                                                    (concat in " => e")))
+           n>
+           (choice* :name "handler-block"
+                    :missing
+                    (@ "nil" n>)
+                    :rest
+                    (r> n>))
+           "end"))
+         (:description
+          "begin ... ensure ... end"
+          :key "bbe"
+          :mark-node t
+          :shorthand calls
+          :name "nest-begin-ensure"
+          :template
+          ((save-column
+            @ "begin" n>
+            (choice* :name "expression-block"
+                     :missing
+                     (@ "nil")
+                     :rest
+                     (r>))
+            n)
+           "ensure" n>
+           (choice* :name "handler-block"
+                    :missing
+                    (@ "nil" n>)
+                    :rest
+                    (r> n>))
+           "end"))
+         (:description
+          "begin ... rescue ... ensure ... end"
+          :key "bre"
+          :mark-node t
+          :shorthand calls
+          :name "nest-begin-rescue-ensure"
+          :template
+          ((save-column
+            @ "begin" n>
+            (choice* :name "expression-block"
+                     :missing
+                     (@ "nil")
+                     :rest
+                     (r>))
+            n)
+           "rescue " (p StandardError "Exception" (lambda (in)
+                                                    (concat in " => e")))
+           n>
+           (choice* :name "handler-block"
+                    :missing
+                    (@ "nil" n>)
+                    :rest
+                    (r> n>))
 
-	   "ensure" n>
-	   (choice* :name "handler-block"
-		    :missing
-		    (@ "nil" n>)
-		    :rest
-		    (r> n>))
-	   "end"))
-	 (:description
-	  "def ... end"
-	  :key "bd"
-	  :mark-node t
-	  :shorthand calls
-	  :name "nest-def"
-	  :template
-	  ("def " (p name "Name") (p none "Arguments" (lambda (in)
-							(if (string= in "none")
-							    ""
-							  (concat "(" in ")"))))
-	   n> r>))
-	 (:description
-	  ".each ..."
-	  :key "be"
-	  :mark-node t
-	  :shorthand iterables
-	  :name "nest-each"
-	  :template
-	  ((save-column r> ".each do |" (p value "Arguments") "|")
-	   n>
-	   @ n>
-	   "end"))
-	 (:description
-	  ".each_with_index ..."
-	  :key "bE"
-	  :mark-node t
-	  :shorthand iterables
-	  :name "nest-each-with-index"
-	  :template
-	  ((save-column r> ".each_with_index do |" (p value "Arguments") "," (p index "Index") "|")
-	   n>
-	   @ n>
-	   "end"))))
+           "ensure" n>
+           (choice* :name "handler-block"
+                    :missing
+                    (@ "nil" n>)
+                    :rest
+                    (r> n>))
+           "end"))
+         (:description
+          "def ... end"
+          :key "bd"
+          :mark-node t
+          :shorthand calls
+          :name "nest-def"
+          :template
+          ("def " (p name "Name") (p none "Arguments" (lambda (in)
+                                                        (if (string= in "none")
+                                                            ""
+                                                          (concat "(" in ")"))))
+           n> r>
+           "end"))
+         (:description
+          ".each ..."
+          :key "be"
+          :mark-node t
+          :shorthand iterables
+          :name "nest-each"
+          :template
+          ((save-column r> ".each do |" (p value "Arguments") "|")
+           n>
+           @ n>
+           "end"))
+         (:description
+          ".each_with_index ..."
+          :key "bE"
+          :mark-node t
+          :shorthand iterables
+          :name "nest-each-with-index"
+          :template
+          ((save-column r> ".each_with_index do |" (p value "Arguments") "," (p index "Index") "|")
+           n>
+           @ n>
+           "end"))))
       (envelope-deindent-function #'combobulate-ruby-envelope-deindent-level)
       (indent-calculate-function #'combobulate-ruby-calculate-indent)
       (procedures-defun
-       '((:activation-nodes ((:nodes ("class" "method" "module" "lambda"))))))
+       '((:activation-nodes ((:nodes ("class" "method" "singleton_method" "singleton_class" "module" "lambda"))))))
       (procedures-sexp
-       '((:activation-nodes ((:nodes ("class" "method" "module" "lambda" "string"))))))
+       '((:activation-nodes ((:nodes ("class" "method" "singleton_method" "singleton_class" "module" "lambda" "string"))))))
       (procedures-sibling
        '((:activation-nodes
-	  ((:nodes
-	    ((rule "keyword_parameter")
-	     (rule "hash_splat_parameter")
-	     ",")
-	    :has-parent ("method_parameters")))
-	  :selector (:match-children t))))
+          ((:nodes
+            ((rule "keyword_parameter")
+             (rule "hash_splat_parameter")
+             (rule "optional_parameter")
+             (rule "splat_parameter")
+             (rule "block_parameter")
+             (rule "forward_parameter")
+             ",")
+            :has-parent ("method_parameters" "parameters" "bare_parameters")))
+          :selector (:match-children t))
+         (:activation-nodes
+          ((:nodes
+            ((rule "pair")
+             (rule "hash_splat_argument")
+             ",")
+            :has-parent ("hash")))
+          :selector (:match-children t))
+         (:activation-nodes
+          ((:nodes
+            ((rule "_arg")
+             (rule "_argument")
+             (rule "splat_argument")
+             (rule "block_argument")
+             (rule "forward_argument")
+             ",")
+            :has-parent ("array" "argument_list" "command_argument_list")))
+          :selector (:match-children t))
+         (:activation-nodes
+          ((:nodes
+            ((rule "_statement")
+             (rule "empty_statement")
+             (rule "_expression"))
+            :has-parent ("block_body" "body_statement" "_statements" "program")))
+          :selector (:match-children t))))
       (procedures-hierarchy
-       '(;; For statements that can be descended into
-	 (:activation-nodes
-	  ((:nodes
-	    ((rule "if")
-	     (rule "unless")
-	     (rule "while")
-	     (rule "until")
-	     (rule "for")
-	     (rule "begin")
-	     (rule "class")
-	     (rule "module")
-	     (rule "method")
-	     "when")
-	    :position at))
-	  :selector (:choose node
-		     :match-children (:match-rules ("block"))))
-	 
-	 ;; Lambda bodies
-	 (:activation-nodes
-	  ((:nodes
-	    ((rule "lambda")
-	     (rule "lambda_literal"))
-	    :position at))
-	  :selector (:choose node
-		     :match-children (:match-rules (rule "lambda" :body))))
-	 
-	 ;; General case
-	 (:activation-nodes
-	  ((:nodes ((all)) :has-parent ((all))))
-	  :selector (:choose node
-		     :match-children (:discard-rules ("block")))))))))
+       '(;; For statements and expressions that can be descended into
+         (:activation-nodes
+          ((:nodes
+            ((rule "if")
+             (rule "unless")
+             (rule "while")
+             (rule "until")
+             (rule "for")
+             (rule "begin")
+             (rule "class")
+             (rule "singleton_class")
+             (rule "module")
+             (rule "method")
+             (rule "singleton_method")
+             (rule "case")
+             (rule "case_match")
+             "when"
+             "in_clause"
+             "rescue"
+             "ensure"
+             "else"
+             "elsif")
+            :position at))
+          :selector (:choose node
+                     :match-children (:match-rules ("block_body" "body_statement" "_statements" "do"))))
+
+         ;; Lambda and block bodies
+         (:activation-nodes
+          ((:nodes
+            ((rule "lambda")
+             (rule "block")
+             (rule "do_block"))
+            :position at))
+          :selector (:choose node
+                     :match-children (:match-rules ("block_body" "_statements"))))
+
+         ;; Call method navigation
+         (:activation-nodes
+          ((:nodes
+            ((rule "call")
+             (rule "command_call")
+             (rule "command_call_with_block")
+             (rule "element_reference"))
+            :position at))
+          :selector (:choose node
+                     :match-children (:match-rules ("argument_list" "command_argument_list" "block" "do_block"))))
+
+         ;; General case for other container nodes
+         (:activation-nodes
+          ((:nodes ((all)) :has-parent ((all))))
+          :selector (:choose node
+                     :match-children (:discard-rules ("block_body" "body_statement" "_statements" "do")))))))))
 
 (define-combobulate-language
  :name ruby
